b5d17c78dc28d5fb1cb14f1381c4be5a9fe4189c
Alec Reeves <areeves@cj.com>
2018-11-15 11:38:24 -0800

    sends message

--- test/Spec/Deploybot/Deploybot.hs
+++ test/Spec/Deploybot/Deploybot.hs
@@ -4,24 +4,41 @@ module Spec.Deploybot.Deploybot (tests) where
 import Test.Tasty.HUnit ((@?=), Assertion, testCase)
 import Test.Tasty (TestTree, testGroup)
 
+--import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Writer
+import Data.Text (Text)
+
 (===) :: (Eq a, Show a) => a -> a -> Assertion
 (===) = (@?=)
 infixl 1 ===
 
 tests :: TestTree
 tests = testGroup "Deploybot" [
-  testCase "initial" $ do
+  testCase "sends message to slack" $ do
     let
-      expected = True
-      actual = False
-    actual === expected
+      messageToSend = "Hey there!"
+
+      actual = runBot
+
+    extractMessage actual === [messageToSend]
  ]
 
-type TestWiring
+type TestWiring = Writer [Text]
+
+instance Slack TestWiring where
+  sendMessage msg = tell [msg]
+
+extractMessage :: TestWiring a -> [Text]
+extractMessage = snd . runWriter
 
 -- above this line test
 ----------------------------------------------------------
 -- below this line prod
 
-runBot :: m ()
-runBot = undefined
+runBot :: (Monad m, Slack m) => m ()
+runBot = do
+  sendMessage "Hey there!"
+  pure ()
+
+class Slack m where
+  sendMessage :: Text -> m ()
